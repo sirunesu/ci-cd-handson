5f267b50e8cf04c6984f8f7af20b3c7218f89493
sirunesu <itou_1996@yahoo.co.jp>
2026-09-15 14:18:05 +0900

    index.jsのタイポを修正(createserver→createServer)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const server = http.createserver((req, res) => {
+const server = http.createServer((req, res) => {
   res.end('Hello from CI/CD handson');
 });
 server.listen(3000, () => console.log('listening on 3000'));
